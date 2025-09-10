c60f468d8314b96aa48f96c8bb96d0729c98c6b2
2501003-k <netstyle_boo@kbu.ac.kr>
2025-09-10 11:29:33 +0900

    2주차

--- week02/Sample7/src/Main.java
+++ week02/Sample7/src/Main.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
-public class Main { //메소드
+public class Main {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
 
-        String name = "홍길동";// 선언문 (준비)
+        //준비물
+        char ch = ' ';
 
-        name = keyboard.nextLine();// Data 입력
+        //입력
+        System.out.print("문자 하나 입력 : ");
+        ch = keyboard.next().charAt(0);
+        //처리
 
-        // 처리
-
-        // 출력 (필수) <- 필수니까 제일 1순위로 적기
-        System.out.println("입력받은 이름 : " + name);
+        //출력
+        System.out.printf("입력한 문자 %c의 ASCII 코드는 %h이다.\n", ch, ch);
     }
 }
